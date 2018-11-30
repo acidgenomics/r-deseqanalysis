4cd568b36b816d5d31c4a4a775c09b9574abd973
Michael Steinbaugh <michael.steinbaugh@constellationpharma.com>
2018-11-30 16:00:39 -0500

    Update NAMESPACE

--- R/package.R
+++ R/package.R
@@ -28,9 +28,9 @@
 #' @importFrom assertthat assert_that
 #' @importFrom basejump Gene2Symbol assertFormalGene2Symbol as_tibble
 #'   basejump_geom_label_repel camel humanize initDir interestingGroups<-
-#'   mapGenesToRownames markdownHeader markdownList matchArgsToDoCall
+#'   makeNames mapGenesToRownames markdownHeader markdownList matchArgsToDoCall
 #'   matchInterestingGroups plotGene plotHeatmap plotPCA sanitizeRowData
-#'   showSlotInfo snake
+#'   showSlotInfo snake standardizeCall
 #' @importFrom cowplot draw_plot ggdraw
 #' @importFrom dplyr arrange desc everything filter left_join mutate pull rename
 #'   row_number select
