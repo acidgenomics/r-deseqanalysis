6ea9400ccecbf85e45cb962daadadc035f159337
Michael Steinbaugh <michael.steinbaugh@constellationpharma.com>
2018-12-03 13:36:38 -0500

    Define `contrastSamples` boolean

--- R/params.R
+++ R/params.R
@@ -1,6 +1,9 @@
 #' @name params
 #' @inherit basejump::params
 #' @keywords internal
+#' @param contrastSamples `boolean`. **Experimental**. Only include the samples
+#'   used to define the contrast passed to `DESeq2::results()`. This setting
+#'   will break for complex DESeq2 contrasts (e.g. interaction effect).
 #' @param lfcShrink `boolean`. Use shrunken log2 fold change (LFC) values.
 #' @param results `scalar`. Position or name of `DESeqResults`.
 NULL
