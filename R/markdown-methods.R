f9107186fb82be127316617f0c18b6de6b4a35d4
Michael Steinbaugh <michael.steinbaugh@constellationpharma.com>
2018-12-04 14:36:15 -0500

    Simplify `markdown()` method support, removing `DESeqResultsTables`

--- R/markdown-methods.R
+++ R/markdown-methods.R
@@ -36,33 +36,3 @@ setMethod(
     signature = signature("DESeqAnalysis"),
     definition = markdown.DESeqAnalysis
 )
-
-
-
-markdown.DESeqResultsTables <-  # nolint
-    function(object, headerLevel = 2L) {
-        assert_that(is(object, "DESeqResultsTables"))
-        validObject(object)
-        assertIsHeaderLevel(headerLevel)
-
-        metadata <- slot(object, "metadata")
-
-        # Include a contrast header, which is useful for looping.
-        contrast <- contrastName(object)
-        markdownHeader(contrast, level = headerLevel, asis = TRUE)
-        headerLevel <- headerLevel + 1L
-
-        # Top tables.
-        markdownHeader("Top tables", level = headerLevel, asis = TRUE)
-        topTables(object)
-    }
-
-
-
-#' @describeIn markdown File paths (if exported) and top tables.
-#' @export
-setMethod(
-    f = "markdown",
-    signature = signature("DESeqResultsTables"),
-    definition = markdown.DESeqResultsTables
-)
