2077562aca46c30dc75ab24cd2bcdf0196ca780c
Michael Steinbaugh <michael.steinbaugh@constellationpharma.com>
2018-12-04 16:50:20 -0500

    Break out internal `.matchResults()` function to a separate file

--- R/DESeq2-internal.R
+++ R/DESeq2-internal.R
@@ -49,38 +49,6 @@
 
 
 
-.matchResults <- function(
-    object,
-    results,
-    lfcShrink = FALSE
-) {
-    assert_is_all_of(object, "DESeqAnalysis")
-    # Default to using the first contrast, for convenience.
-    if (missing(results)) {
-        results <- 1L
-    }
-    assert_is_scalar(results)
-    assert_is_a_bool(lfcShrink)
-    if (isTRUE(lfcShrink)) {
-        slotName <- "lfcShrink"
-    } else {
-        slotName <- "results"
-    }
-    results <- slot(object, name = slotName)[[results]]
-    assert_is_all_of(results, "DESeqResults")
-
-    # Inform the user about which data we're using.
-    msg <- paste("DESeqResults:", contrastName(results))
-    if (isTRUE(lfcShrink)) {
-        msg <- paste(msg, "(shrunken LFC)")
-    }
-    message(msg)
-
-    results
-}
-
-
-
 .transformCountsAxisLabel <- function(object) {
     paste(.transformType(object), "counts (log2)")
 }
