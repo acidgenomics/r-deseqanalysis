a54b7b503b1e8e96a858f4adffaf23a9e0cbb850
Michael Steinbaugh <michael.steinbaugh@constellationpharma.com>
2018-12-12 12:16:48 -0500

    Update assert checks

--- R/matchResults-internal.R
+++ R/matchResults-internal.R
@@ -4,13 +4,15 @@
     results,
     lfcShrink = TRUE
 ) {
-    assert_is_all_of(object, "DESeqAnalysis")
+    assert(is(object, "DESeqAnalysis"))
     # Default to using the first contrast, for convenience.
     if (missing(results)) {
         results <- 1L
     }
-    assert_is_scalar(results)
-    assert_is_a_bool(lfcShrink)
+    assert(
+        isScalar(results),
+        isFlag(lfcShrink)
+    )
     # Match shrunken results by default.
     if (
         isTRUE(lfcShrink) &&
@@ -21,7 +23,7 @@
         slotName <- "results"
     }
     results <- slot(object, name = slotName)[[results]]
-    assert_is_all_of(results, "DESeqResults")
+    assert(is(results, "DESeqResults"))
 
     # Inform the user about which data we're using.
     msg <- paste("DESeqResults:", contrastName(results))
