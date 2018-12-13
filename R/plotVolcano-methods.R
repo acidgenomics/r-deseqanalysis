991e36dc27d0543c908ee7a5fd5d862f9cead731
Michael Steinbaugh <michael.steinbaugh@constellationpharma.com>
2018-12-13 11:37:13 -0500

    Restrict exported method support to DESeqAnalysis, removing legacy DESeqResults method support

--- R/plotVolcano-methods.R
+++ R/plotVolcano-methods.R
@@ -21,16 +21,18 @@
 #' print(genes)
 #'
 #' ## DESeqAnalysis ====
-#' plotVolcano(deseq)
+#' plotVolcano(deseq, results = 1L)
 #'
 #' ## Customize the colors.
 #' plotVolcano(
 #'     object = deseq,
+#'     results = 1L,
 #'     pointColor = "black",
 #'     sigPointColor = "purple"
 #' )
 #' plotVolcano(
 #'     object = deseq,
+#'     results = 1L,
 #'     sigPointColor = c(
 #'         upregulated = "green",
 #'         downregulated = "red"
@@ -40,18 +42,20 @@
 #' ## Directional support (up or down).
 #' plotVolcano(
 #'     object = deseq,
+#'     results = 1L,
 #'     direction = "up",
 #'     ntop = 5L
 #' )
 #' plotVolcano(
 #'     object = deseq,
+#'     results = 1L,
 #'     direction = "down",
 #'     ntop = 5L
 #' )
 #'
 #' ## Label genes manually.
 #' ## Note that either gene IDs or names (symbols) are supported.
-#' plotVolcano(deseq, genes = genes)
+#' plotVolcano(deseq, results = 1L, genes = genes)
 NULL
 
 
@@ -307,16 +311,6 @@ plotVolcano.DESeqResults <-  # nolint
 
 
 
-#' @rdname plotVolcano
-#' @export
-setMethod(
-    f = "plotVolcano",
-    signature = signature("DESeqResults"),
-    definition = plotVolcano.DESeqResults
-)
-
-
-
 plotVolcano.DESeqAnalysis <-  # nolint
     function(
         object,
@@ -325,7 +319,7 @@ plotVolcano.DESeqAnalysis <-  # nolint
     ) {
         validObject(object)
         do.call(
-            what = plotVolcano,
+            what = plotVolcano.DESeqResults,
             args = matchArgsToDoCall(
                 args = list(
                     object = .matchResults(
