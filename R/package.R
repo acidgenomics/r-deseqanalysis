dc8d0d1321eb8f1c0f572d50a7e14ee0fe28b9e6
Michael Steinbaugh <mike@steinbaugh.com>
2018-12-21 14:27:23 -0500

    Update NAMESPACE

--- R/package.R
+++ R/package.R
@@ -29,13 +29,13 @@
 #' @importFrom ggplot2 aes annotation_logticks element_blank geom_density
 #'   geom_hline geom_point geom_vline ggplot guides labs scale_color_manual
 #'   scale_x_continuous scale_y_continuous theme
-#' @importFrom goalie areDisjointSets assert containsAlpha containsHeaderLevel
-#'   hasLength hasNames hasLength hasRows hasRownames hasValidDimnames isAny
-#'   isCharacter isFlag isInRange isInt isNonEmpty isNonNegative isNumber
-#'   isPositive isScalar isString isSubset validate
+#' @importFrom goalie areDisjointSets assert bapply hasLength hasNames hasLength
+#'   hasRows hasRownames hasValidDimnames isAlpha isAny isCharacter isFlag
+#'   isHeaderLevel isInRange isInt isNonEmpty isNonNegative isNumber isPositive
+#'   isScalar isString isSubset validate
 #' @importFrom knitr kable
 #' @importFrom magrittr %>%
-#' @importFrom methods as is new slot slot<- validObject
+#' @importFrom methods as is new prototype setClass slot slot<- validObject
 #' @importFrom rlang := UQ sym syms
 #' @importFrom scales pretty_breaks
 #' @importFrom stringr str_match str_trunc
