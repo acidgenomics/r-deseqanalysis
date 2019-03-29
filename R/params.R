6e380030e5b17333c46de8ce5f5b6b37659157dd
Michael Steinbaugh <mike@steinbaugh.com>
2019-03-29 09:20:52 -0400

    Update params

--- R/params.R
+++ R/params.R
@@ -5,6 +5,10 @@
 #' @name params
 #' @inherit basejump::params
 #' @keywords internal
+#'
+#' @param alpha `numeric(1)`.
+#'   Adjusted P value ("alpha") cutoff. If left `NULL`, will use the cutoff
+#'   defined in the object.
 #' @param contrastSamples `logical(1)`.
 #'   **Experimental**. Only include the samples used to define the contrast
 #'   passed to [DESeq2::results()]. This setting will break for complex DESeq2
@@ -13,6 +17,9 @@
 #'   Plot "`both`", "`up`", or "`down`" directions.
 #' @param lfcShrink `logical(1)`.
 #'   Use shrunken log2 fold change (LFC) values.
+#' @param lfcThreshold `numeric(1)`.
+#'   Log fold change ratio (base 2) cutoff threshold. If left `NULL`, will use
+#'   the cutoff defined in the object.
 #' @param results `character(1)` or `integer(1)`.
 #'   Name or position of `DESeqResults`.
 #' @param pointColor `character(1)`.
