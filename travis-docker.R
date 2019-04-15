7f18848764a588965ba8bc0ea9fe15f3afabcf24
Michael Steinbaugh <mike@steinbaugh.com>
2019-04-15 16:14:15 -0400

    Workaround to prevent BiocCheck error on missing R Markdown child files

--- travis-docker.R
+++ travis-docker.R
@@ -1,13 +1,3 @@
 rcmdcheck::rcmdcheck(args = "--no-manual")
-file.create(
-    file.path(
-        "inst",
-        "rmarkdown",
-        "templates",
-        "differential-expression",
-        "skeleton",
-        c("_footer.Rmd", "_header.Rmd", "_links.Rmd")
-    )
-)
 BiocCheck::BiocCheck()
 lintr::lint_package()
