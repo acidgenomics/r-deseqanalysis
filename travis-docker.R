3585ac771aa3cc6635542eeb0d50cae020e01e25
Michael Steinbaugh <mike@steinbaugh.com>
2019-04-15 14:36:38 -0400

    Prepare for release

--- travis-docker.R
+++ travis-docker.R
@@ -1,3 +1,13 @@
 rcmdcheck::rcmdcheck(args = "--no-manual")
+file.create(
+    file.path(
+        "inst",
+        "rmarkdown",
+        "templates",
+        "differential-expression",
+        "skeleton",
+        c("_footer.Rmd", "_header.Rmd", "_links.Rmd")
+    )
+)
 BiocCheck::BiocCheck()
 lintr::lint_package()
