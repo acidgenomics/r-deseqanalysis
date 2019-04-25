ad59fc52589c212d0b26357e418123ec5d64495b
Michael Steinbaugh <mike@steinbaugh.com>
2019-04-25 09:14:43 -0400

    Rework to support `format` argument

--- R/contrastName-methods.R
+++ R/contrastName-methods.R
@@ -2,6 +2,16 @@
 #' @inherit bioverbs::contrastName
 #' @inheritParams basejump::params
 #' @inheritParams params
+#'
+#' @param format `character(1)`.
+#'   Name format to return:
+#'
+#'   - `resultsNames`: Attempt to matching the conventions in
+#'     [`resultsNames()`][DESeq2::resultsNames].
+#'   - `title`: Human readable, for plot titles and/or table captions.
+#'
+#' @seealso [`resultsNames()`][DESeq2::resultsNames].
+#'
 #' @examples
 #' data(deseq)
 #'
@@ -24,18 +34,25 @@ NULL
 
 
 contrastName.DESeqResults <-  # nolint
-    function(object) {
+    function(object, format = c("resultsNames", "title")) {
         validObject(object)
-        x <- mcols(object)[2L, "description"]
+        format <- match.arg(format)
+        x <- mcols(object)["log2FoldChange", "description", drop = TRUE]
         assert(isCharacter(x))
-        x %>%
-            # Strip prefix, e.g. log2 fold change (MLE).
-            sub("^.*:\\s", "", .) %>%
-            # Pad the first space with as a colon.
-            sub("\\s", " : ", .) %>%
-            sub("\\svs\\s", " vs. ", .) %>%
-            # Improve appearance for difference of differences.
-            gsub("\\+", " \\+\n    ", .)
+        # Always strip prefix, e.g. log2 fold change (MLE).
+        x <- sub("^.*:\\s", "", x)
+        if (format == "resultsNames") {
+            makeNames(x)
+        } else if (format == "title") {
+            x %>%
+                # Strip prefix, e.g. log2 fold change (MLE).
+                sub("^.*:\\s", "", .) %>%
+                # Pad the first space with as a colon.
+                sub("\\s", " : ", .) %>%
+                sub("\\svs\\s", " vs. ", .) %>%
+                # Improve appearance for difference of differences.
+                gsub("\\+", " \\+\n    ", .)
+        }
     }
 
 
