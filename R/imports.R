43fbab82ecaa2177448121eae50ea57414f82dde
Michael Steinbaugh <mike@steinbaugh.com>
2019-08-20 09:26:59 -0400

    Work on making the imports lighter

--- R/imports.R
+++ R/imports.R
@@ -1,3 +1,7 @@
+## Note that `summary.DESeqResults()` isn't currently exported in BioC 3.10.
+
+
+
 #' @importClassesFrom DESeq2 DESeqDataSet DESeqTransform
 #' @importClassesFrom SummarizedExperiment RangedSummarizedExperiment
 #'   SummarizedExperiment
@@ -18,8 +22,6 @@
 #'   matchesGene2Symbol methodFormals printString removeNA showSlotInfo
 #'   standardizeCall
 #' @importFrom cowplot draw_plot ggdraw
-#' @importFrom dplyr arrange desc everything filter left_join mutate mutate_all
-#'   pull rename row_number select
 #' @importFrom ggplot2 aes annotation_logticks element_blank geom_density
 #'   geom_hline geom_point geom_vline ggplot guides labs scale_colour_manual
 #'   scale_x_continuous scale_y_continuous theme
@@ -32,11 +34,14 @@
 #' @importFrom methods as is new prototype setClass show slot slot<- validObject
 #'   .hasSlot
 #' @importFrom acidplots acid_geom_label_repel plotCounts plotPCA
-#' @importFrom rlang !! := UQ sym syms
 #' @importFrom scales log_breaks pretty_breaks
 #' @importFrom stringr str_match str_trunc
-#' @importFrom tidyselect starts_with
 #' @importFrom utils capture.output getS3method globalVariables packageVersion
+#'
+#'
+#'
+#' @importFrom dplyr arrange desc everything filter left_join mutate mutate_all
+#'   pull rename row_number select
+#' @importFrom rlang !! := UQ sym syms
+#' @importFrom tidyselect starts_with
 NULL
-
-## Note that `summary.DESeqResults()` isn't currently exported in BioC 3.10.
