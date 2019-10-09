caa779150eec14cae21f9d42a0de778a90a3114a
Michael Steinbaugh <mike@steinbaugh.com>
2019-10-09 13:06:55 -0400

    Rework function to be more metadata-centric. Ensure rows never rearrange

--- tests/testthat/test-matchMetadataToFiles.R
+++ tests/testthat/test-matchMetadataToFiles.R
@@ -1,11 +1,6 @@
-context("matchFilesToMetadata")
+context("matchMetadataToFiles")
 
 test_that("Kebab files, snake user metadata out of order", {
-    files <- file.path(
-        "salmon",
-        paste(seq_len(4L), "sample", LETTERS[seq_len(4L)], sep = "-"),
-        "quant.sf"
-    )
     metadata <- data.frame(
         sampleID = paste(
             seq_len(4L),
@@ -16,7 +11,12 @@ test_that("Kebab files, snake user metadata out of order", {
         condition = rep(LETTERS[seq_len(2L)], times = 2L)
     )
     metadata <- metadata[nrow(metadata):1L, ]
-    object <- matchFilesToMetadata(files = files, metadata = metadata)
+    files <- file.path(
+        "salmon",
+        paste(seq_len(4L), "sample", LETTERS[seq_len(4L)], sep = "-"),
+        "quant.sf"
+    )
+    object <- matchMetadataToFiles(metadata = metadata, files = files)
     expected <- data.frame(
         files = basename(dirname(files)),
         metadata = sort(metadata[[1L]])
@@ -25,23 +25,23 @@ test_that("Kebab files, snake user metadata out of order", {
 })
 
 test_that("Error on unresolvable mismatch", {
+    metadata <- data.frame(sampleID = "sample_1")
     files <- file.path(
         "salmon",
         paste("sample", seq_len(2L), sep = "-"),
         "quant.sf"
     )
-    metadata <- data.frame(sampleID = "sample_1")
     expect_error(
-        object = matchFilesToMetadata(files, metadata),
+        object = matchMetadataToFiles(metadata = metadata, files = files),
         regexp = "Match failure"
     )
 })
 
 test_that("Non-tximport quant file input", {
-    files <- "sample-1.fastq.gz"
     metadata <- data.frame(sampleID = "sample_1")
+    files <- "sample-1.fastq.gz"
     expect_error(
-        object = matchFilesToMetadata(files, metadata),
+        object = matchMetadataToFiles(metadata = metadata, files = files),
         regexp = "Failed to detect sample name from quant file."
     )
 })
