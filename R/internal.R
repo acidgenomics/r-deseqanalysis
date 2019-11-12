dc6e488a6e68c30a850a601eb40cf8226ce196cf
Michael Steinbaugh <mike@steinbaugh.com>
2019-11-12 15:56:49 -0500

    Consolidate internal functions

--- R/internal.R
+++ R/internal.R
@@ -65,7 +65,7 @@
 #'
 #' @examples
 #' data <- .usefulRowData(object)
-.usefulRowData <- function(object) {
+.extractUsefulRowData <- function(object) {
     if (is(object, "DESeqAnalysisList")) {
         object <- object[[1L]]
     }
@@ -78,6 +78,110 @@
     )
     keep <- !bapply(X = data, FUN = isAny, classes = c("logical", "numeric"))
     out <- data[, keep, drop = FALSE]
+    out
+}
+
+
+
+## Updated 2019-11-12.
+.joinCounts <- function(
+    object,  # nolint
+    DESeqDataSet   # nolint
+) {
+    assert(
+        is(object, "DataFrame"),
+        is(DESeqDataSet, "DESeqDataSet"),
+        identical(
+            x = rownames(object),
+            y = rownames(DESeqDataSet)
+        ),
+        areDisjointSets(
+            x = colnames(object),
+            y = colnames(DESeqDataSet)
+        )
+    )
+    validObject(object)
+    validObject(DESeqDataSet)
+    message("Joining size factor adjusted normalized counts.")
+    counts <- counts(DESeqDataSet, normalized = TRUE)
+    out <- cbind(object, counts)
+    ## Ensure we're not changing the object class on return.
+    ## This can happen for DESeqResults, which will coerce to DataFrame.
+    if (!identical(x = class(object), y = class(out))) {
+        out <- as(out, Class = class(object)[[1L]])
+    }
+    validObject(out)
+    out
+}
+
+
+
+## Join the row annotations.
+##
+## DESeq2 includes additional columns in `rowData()` that aren't informative for
+## a user, and doesn't need to be included in the tables. Instead, only keep
+## informative columns that are character or factor. Be sure to drop complex,
+## non-atomic columns (e.g. list, S4) that are allowed in GRanges/DataFrame but
+## will fail to write to disk as CSV. Note that we're using `decode()` here to
+## handle S4 Rle columns from the Genomic Ranges.
+##
+## Updated 2019-11-12.
+.joinRowData <- function(
+    object,  # nolint
+    DESeqDataSet   # nolint
+) {
+    assert(
+        is(object, "DataFrame"),
+        is(DESeqDataSet, "DESeqDataSet"),
+        identical(
+            x = rownames(object),
+            y = rownames(DESeqDataSet)
+        ),
+        areDisjointSets(
+            x = colnames(object),
+            y = colnames(DESeqDataSet)
+        )
+    )
+    validObject(object)
+    validObject(DESeqDataSet)
+    message("Joining row annotations.")
+    ## SummarizedExperiment inconsistently handles rownames on rowData.
+    ## Ensure they are set here before continuing.
+    rownames <- rownames(DESeqDataSet)
+    rowData <- rowData(DESeqDataSet)
+    rownames(rowData) <- rownames
+    rowData <- decode(rowData)
+    keep <- vapply(
+        X = rowData,
+        FUN = function(x) {
+            is.character(x) || is.factor(x)
+        },
+        FUN.VALUE = logical(1L)
+    )
+    if (!any(keep)) {
+        stop(
+            "No suitable row annotations detected.\n",
+            "Check 'rowData()' of DESeqDataSet."
+        )
+    }
+    rowData <- rowData[, keep, drop = FALSE]
+    assert(
+        all(vapply(
+            X = rowData,
+            FUN = is.atomic,
+            FUN.VALUE = logical(1L)
+        )),
+        hasLength(rowData),
+        identical(rownames(object), rownames(rowData)),
+        areDisjointSets(colnames(object), colnames(rowData))
+    )
+    out <- cbind(object, rowData)
+    ## Ensure we're not changing the object class on return.
+    ## This can happen for DESeqResults, which will coerce to DataFrame.
+    if (!identical(x = class(object), y = class(out))) {
+        out <- as(out, Class = class(object)[[1L]])
+    }
+    validObject(out)
     out
 }
 
