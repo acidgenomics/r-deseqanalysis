dfd3ea4609f32e6bb233b733ab1233cb98cb6e77
Michael Steinbaugh <mike@steinbaugh.com>
2020-07-28 06:31:34 -0400

    Simplify parameterized unit tests

--- tests/testthat/test-prepareTximportFiles.R
+++ tests/testthat/test-prepareTximportFiles.R
@@ -1,24 +1,26 @@
 context("prepareTximportFiles")
 
-makeNames <- eval(formals(prepareTximportFiles)[["makeNames"]])
-with_parameters_test_that(
-    "makeNames", {
-        files <- c(
-            file.path("salmon", "1-sample-A", "quant.sf"),
-            file.path("salmon", "100-sample-B", "quant.sf")
-        )
-        object <- prepareTximportFiles(
-            files = files,
-            makeNames = makeNames,
-            exists = FALSE
-        )
-        expect_identical(unname(object), files)
-        expect_identical(names(object), names)
-    },
-    makeNames = makeNames,
-    names = list(
-        makeNames = c("X001_sample_A", "X100_sample_B"),
-        snakeCase = c("x001_sample_a", "x100_sample_b"),
-        camelCase = c("x001SampleA", "x100SampleB")
+test_that("makeNames", {
+    mapply(
+        makeNames = eval(formals(prepareTximportFiles)[["makeNames"]]),
+        names = list(
+            makeNames = c("X001_sample_A", "X100_sample_B"),
+            snakeCase = c("x001_sample_a", "x100_sample_b"),
+            camelCase = c("x001SampleA", "x100SampleB")
+        ),
+        FUN = functinon(makeNames, names) {
+            files <- c(
+                file.path("salmon", "1-sample-A", "quant.sf"),
+                file.path("salmon", "100-sample-B", "quant.sf")
+            )
+            object <- prepareTximportFiles(
+                files = files,
+                makeNames = makeNames,
+                exists = FALSE
+            )
+            expect_identical(unname(object), files)
+            expect_identical(names(object), names)
+        },
+        SIMPLIFY = FALSE
     )
-)
+})
