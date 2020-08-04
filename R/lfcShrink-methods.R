734b70e56741ca9f394005b50880de878d084e60
Michael Steinbaugh <mike@steinbaugh.com>
2020-08-04 11:38:51 -0400

    Create assignment method

--- R/lfcShrink-methods.R
+++ R/lfcShrink-methods.R
@@ -32,3 +32,49 @@ setMethod(
     signature = signature("DESeqDataSet"),
     definition = `lfcShrink,DESeqDataSet`
 )
+
+
+
+## Updated 2020-08-04.
+`lfcShrink,DESeqAnalysis` <-  # nolint
+    function(object) {
+        validObject(object)
+        ok <- metadata(object)[["lfcShrink"]]
+        if (is.logical(ok)) return(ok)
+        ok <- hasLength(slot(object, "lfcShrink"))
+        ok
+    }
+
+
+
+#' @rdname lfcShrink
+#' @export
+setMethod(
+    f = "lfcShrink",
+    signature = signature("DESeqAnalysis"),
+    definition = `lfcShrink,DESeqAnalysis`
+)
+
+
+
+## Updated 2020-08-04.
+`lfcShrink<-,DESeqAnalysis,logical` <-  # nolint
+    function(object, value) {
+        assert(isFlag(value))
+        metadata(object)[["lfcShrink"]] <- value
+        validObject(object)
+        object
+    }
+
+
+
+#' @rdname lfcShrink
+#' @export
+setReplaceMethod(
+    f = "lfcShrink",
+    signature = signature(
+        object = "DESeqAnalysis",
+        value = "logical"
+    ),
+    definition = `lfcShrink<-,DESeqAnalysis,logical`
+)
