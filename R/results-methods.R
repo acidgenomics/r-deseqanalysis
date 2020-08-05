8fec2c172d007f0dc735e96c392a517c15fef552
Michael Steinbaugh <mike@steinbaugh.com>
2020-08-05 08:13:33 -0400

    Tighten up results function

--- R/results-methods.R
+++ R/results-methods.R
@@ -47,48 +47,36 @@ setMethod(
 
 
 
-## Updated 2020-08-04.
+## Updated 2020-08-05.
 `results,DESeqAnalysis` <-  # nolint
-    function(
-        object,
-        i,
-        lfcShrink = NULL,
-        ...
-    ) {
-        if (is.null(lfcShrink)) {
-            lfcShrink <- lfcShrink(object)
-        }
-        assert(
-            is(object, "DESeqAnalysis"),
-            isScalar(i),
-            isFlag(lfcShrink)
-        )
+    function(object, i) {
+        validObject(object)
+        assert(isScalar(i))
         if (isCharacter(i)) {
             assert(isSubset(i, resultsNames(object)))
         }
-        ## Match the results.
+        lfcShrink <- lfcShrink(object)
         if (identical(lfcShrink, FALSE)) {
             slotName <- "results"
         } else if (
             isTRUE(lfcShrink) &&
-            hasLength(object@lfcShrink)
+            hasLength(slot(object, name = "lfcShrink"))
         ) {
             slotName <- "lfcShrink"
         } else if (
             isTRUE(lfcShrink) &&
-            !hasLength(object@lfcShrink)
+            !hasLength(slot(object, name = "lfcShrink"))
         ) {
             stop(
                 "Shrunken LFC values were requested, ",
                 "but object does not contain DESeqResults ",
                 "defined in 'lfcShrink' slot.\n",
-                "Set 'lfcShrink = FALSE'."
+                "Set 'lfcShrink(object) <- FALSE'."
             )
         }
         resultsList <- slot(object, name = slotName)
         data <- resultsList[[i]]
         assert(is(data, "DESeqResults"))
-        ## Slot the contrast name into DESeqResults metadata.
         name <- contrastName(object, i = i)
         contrastName(data) <- name
         msg <- name
