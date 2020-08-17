70ec5fc4d8a5d36479580b38e8226962d76c4677
Michael Steinbaugh <mike@steinbaugh.com>
2020-08-17 16:03:53 -0400

    Simplify accepted arguments

--- R/imports.R
+++ R/imports.R
@@ -17,7 +17,7 @@
 #'   convertSampleIDsToNames decode droplevels import initDir
 #'   interestingGroups<- leftJoin makeNames mapGenesToRownames markdownHeader
 #'   markdownList matchesGene2Symbol melt metadata2 metadata2<- printString
-#'   realpath removeNA showSlotInfo snakeCase standardizeCall
+#'   realpath removeNA requireNamespaces showSlotInfo snakeCase standardizeCall
 #' @importFrom cli cli_alert cli_alert_info cli_alert_warning cli_dl
 #' @importFrom cowplot draw_plot ggdraw
 #' @importFrom ggplot2 aes annotation_logticks element_blank geom_bar
