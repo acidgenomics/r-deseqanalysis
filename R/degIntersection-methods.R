52cf27da588197ecdd6b34b7af8113ba6a6a1d9e
Michael Steinbaugh <mike@steinbaugh.com>
2020-10-28 13:48:56 -0400

    Improve `degIntersection` return options

--- R/degIntersection-methods.R
+++ R/degIntersection-methods.R
@@ -1,6 +1,6 @@
 #' @name degIntersection
 #' @inherit AcidGenerics::degIntersection
-#' @note Updated 2020-08-18.
+#' @note Updated 2020-10-28.
 #'
 #' @inheritParams AcidRoxygen::params
 #' @param i `character`, `numeric`, or `NULL`.
@@ -10,7 +10,15 @@
 #'   Include "up" or "down" directions.
 #'   Must be directional, and intentionally does not support "both".
 #' @param return `character(1)`.
-#'   Return intersection matrix, count per contrast, or ratio.
+#'   - `"matrix"`: `logical matrix`;
+#'     Intersection matrix.
+#'   - `"count"`: `integer`;
+#'     Number of times the gene is significant across contrasts.
+#'   - `"ratio"`: `numeric`;
+#'     The ratio of how many times the gene is significant across contrasts.
+#'   - `"names"`: `character`;
+#'     Names of genes that intersect across all contrasts defined.
+#'     Input of specific contrasts with `i` argument is recommended here.
 #' @param ... Passthrough arguments to [deg()].
 #'
 #' @examples
@@ -18,12 +26,16 @@
 #'
 #' ## DESeqAnalysis ====
 #' mat <- degIntersection(deseq, return = "matrix")
+#' class(mat)
+#' type(mat)
 #' head(mat)
 #'
 #' count <- degIntersection(deseq, return = "count")
+#' class(count)
 #' head(count)
 #'
 #' ratio <- degIntersection(deseq, return = "ratio")
+#' class(ratio)
 #' head(ratio)
 NULL
 
@@ -38,13 +50,13 @@ NULL
 
 
 
-## Updated 2020-08-18.
+## Updated 2020-10-28.
 `degIntersection,DESeqAnalysis` <-  # nolint
     function(
         object,
         i = NULL,
         direction = c("up", "down"),
-        return = c("matrix", "count", "ratio")
+        return = c("matrix", "count", "ratio", "names")
     ) {
         direction <- match.arg(direction)
         return <- match.arg(return)
@@ -63,13 +75,22 @@ NULL
             "Returning intersection %s of %s %s-regulated DEGs.",
             return, nrow(mat), direction
         ))
-        count <- sort(rowSums(mat), decreasing = TRUE)
-        switch(
+        count <- rowSums(mat)
+        mode(count) <- "integer"
+        out <- switch(
             EXPR = return,
             "matrix" = mat,
             "count" = count,
-            "ratio" = count / length(list)
+            "ratio" = {
+                count / length(list)
+            },
+            "names" = {
+                x <- apply(X = mat, MARGIN = 1L, FUN = all)
+                x <- names(x)[x]
+                x
+            }
         )
+        out
     }
 
 
