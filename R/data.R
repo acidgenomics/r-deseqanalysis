d30e551bb1d4a917e5339650a5b44809a4258d13
Michael Steinbaugh <mike@steinbaugh.com>
2021-03-08 16:59:32 -0500

    Add dds_list data

--- R/data.R
+++ R/data.R
@@ -1,3 +1,15 @@
+#' List of DESeqDataSet example objects
+#'
+#' @author Michael Steinbaugh
+#' @note Updated 2021-03-08.
+#'
+#' @examples
+#' data(dds_list)
+#' dds_list
+"dds_list"
+
+
+
 #' DESeq2 example analysis
 #'
 #' Minimal DESeq2 differential expression analysis.
