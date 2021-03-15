63a12c53da2a805e26ab758f46738370977d6288
Michael Steinbaugh <mike@steinbaugh.com>
2021-03-15 11:00:49 -0400

    Update unit tests

--- tests/testthat/test-DESeqAnalysisList.R
+++ tests/testthat/test-DESeqAnalysisList.R
@@ -1,6 +1,6 @@
 context("DESeqAnalysisList")
 
-test_that("DESeqAnalysisList", {
-    x <- DESeqAnalysisList(deseq)
+test_that("list", {
+    x <- DESeqAnalysisList(list("object1" = deseq, "object2" = deseq))
     expect_s4_class(x, "DESeqAnalysisList")
 })
