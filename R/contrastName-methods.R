b662df2238dffcada9005e8b834e4bdc438faf5d
Michael Steinbaugh <mike@steinbaugh.com>
2021-03-15 10:48:56 -0400

    Simplify and harden methods

--- R/contrastName-methods.R
+++ R/contrastName-methods.R
@@ -1,23 +1,18 @@
 #' @name contrastName
 #' @inherit AcidGenerics::contrastName
-#' @note Updated 2021-03-12.
+#' @note Updated 2021-03-15.
 #'
 #' @inheritParams AcidRoxygen::params
 #' @inheritParams params
-#' @param .format `character(1)`.
+#' @param format `character(1)`.
 #'   Name format to return:
-#'
 #'   - `resultsNames`: Attempt to matching the conventions in
 #'     [`resultsNames()`][DESeq2::resultsNames].
 #'   - `title`: Human readable, for plot titles and/or table captions.
-#' @param .useStash `logical(1)`.
-#'   Check for `contrastName` metadata stash in `DESeqResults` object. Intended
-#'   for use with `DESeqAnalysis` methods.
 #' @param ... Additional arguments.
 #'
 #' @seealso
-#' - [`contrastNames()`].
-#' - [`resultsNames()`][DESeq2::resultsNames].
+#' - [`resultsNames()`].
 #'
 #' @examples
 #' data(deseq)
@@ -28,26 +23,31 @@ NULL
 
 
 
-## Updated 2020-08-04.
+## This method is to be used primarily to set the contrast name on DESeqResults
+## inside plotting functions. See `plotMA()` method, for example.
+## Updated 2021-03-15.
+`contrastName,DESeqAnalysis` <-  # nolint
+    function(object, i, ...) {
+        assert(isScalar(i))
+        resList <- DESeqResultsList(object, quiet = TRUE)
+        res <- resList[[i]]
+        assert(is(res, "DESeqResults"))
+        contrastName(res, ...)
+    }
+
+
+
+## Updated 2021-03-15.
 `contrastName,DESeqResults` <-  # nolint
     function(
         object,
-        ## FIXME Rethink this.
-        .format = c("resultsNames", "title"),
-        ## FIXME Rethink this.
-        ## FIXME Take this out once we update contrastSamples.
-        .useStash = TRUE
+        format = c("resultsNames", "title")
     ) {
         validObject(object)
-        assert(isFlag(.useStash))
-        .format <- match.arg(.format)
+        format <- match.arg(format)
         ## Use metadata stash, if defined. This is the recommended approach
         ## when passing off from DESeqAnalysis object, using `resultsNames()`.
-        if (isTRUE(.useStash)) {
-            x <- metadata(object)[["contrastName"]]
-        } else {
-            x <- NULL
-        }
+        x <- metadata(object)[["contrastName"]]
         ## Otherwise, determine the contrast name automatically from mcols.
         ## See approach in `DESeq2::resultsNames()` on DESeqDataSet.
         if (is.null(x)) {
@@ -57,30 +57,24 @@ NULL
         assert(isString(x))
         ## Always strip prefix, e.g. log2 fold change (MLE).
         x <- sub("^.*:\\s", "", x)
-        if (identical(.format, "resultsNames")) {
-            x <- makeNames(x)
-        } else if (identical(.format, "title")) {
-            ## Strip prefix, e.g. log2 fold change (MLE).
-            x <- sub("^.*:\\s", "", x)
-            ## Pad the first space with as a colon.
-            x <- sub("\\s", " : ", x)
-            ## Ensure "vs." contains a period.
-            x <- sub("\\svs\\s", " vs. ", x)
-            ## Improve appearance for difference of differences.
-            x <- gsub("\\+", " \\+\n    ", x)
-        }
+        switch(
+            EXPR = format,
+            "resultsNames" = {
+                x <- makeNames(x)
+            },
+            "title" = {
+                ## Strip prefix, e.g. log2 fold change (MLE).
+                x <- sub("^.*:\\s", "", x)
+                ## Pad the first space with as a colon.
+                x <- sub("\\s", " : ", x)
+                ## Ensure "vs." contains a period.
+                x <- sub("\\svs\\s", " vs. ", x)
+                ## Improve appearance for difference of differences.
+                x <- gsub("\\+", " \\+\n    ", x)
+            }
+        )
         x
     }
-
-
-
-#' @rdname contrastName
-#' @export
-setMethod(
-    f = "contrastName",
-    signature = signature("DESeqResults"),
-    definition = `contrastName,DESeqResults`
-)
 
 
 
@@ -97,32 +91,11 @@ setMethod(
 
 #' @rdname contrastName
 #' @export
-setReplaceMethod(
+setMethod(
     f = "contrastName",
-    signature = signature(
-        object = "DESeqResults",
-        value = "character"
-    ),
-    definition = `contrastName<-,DESeqResults,character`
+    signature = signature("DESeqAnalysis"),
+    definition = `contrastName,DESeqAnalysis`
 )
-
-
-
-## This method is to be used primarily to set the contrast name on DESeqResults
-## inside plotting functions. See `plotMA()` method, for example.
-## Updated 2020-08-04.
-`contrastName,DESeqAnalysis` <-  # nolint
-    function(object, i, ...) {
-        contrastNames <- contrastNames(object)
-        if (isString(i)) {
-            x <- i
-            assert(isSubset(x, contrastNames))
-        } else {
-            x <- contrastNames[[i]]
-        }
-        assert(isString(x))
-        x
-    }
 
 
 
@@ -130,6 +103,19 @@ setReplaceMethod(
 #' @export
 setMethod(
     f = "contrastName",
-    signature = signature("DESeqAnalysis"),
-    definition = `contrastName,DESeqAnalysis`
+    signature = signature("DESeqResults"),
+    definition = `contrastName,DESeqResults`
+)
+
+
+
+#' @rdname contrastName
+#' @export
+setReplaceMethod(
+    f = "contrastName",
+    signature = signature(
+        object = "DESeqResults",
+        value = "character"
+    ),
+    definition = `contrastName<-,DESeqResults,character`
 )
