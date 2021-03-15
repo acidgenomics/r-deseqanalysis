18f300bae8e76854aa833aac97abb895825deae1
Michael Steinbaugh <mike@steinbaugh.com>
2021-03-15 15:51:09 -0400

    Improve limit approach for `plotMA` and `plotVolcano`

--- R/package.R
+++ R/package.R
@@ -30,11 +30,47 @@
 #'   geom_density geom_hline geom_point geom_text geom_vline ggplot guides labs
 #'   position_stack scale_color_manual scale_x_continuous scale_y_continuous
 #'   theme
-#' @importFrom goalie areDisjointSets areIntersectingSets areSameLength
-#'   areSetEqual assert bapply hasLength hasNames hasLength hasRows hasRownames
-#'   hasValidDimnames hasValidNames isAFile isAlpha isAny isCharacter isFlag
-#'   isGGScale isHeaderLevel isInRange isInt isNonNegative isNumber isPercentage
-#'   isPositive isScalar isString isSubset validate
+#' @importFrom goalie allArePositive areDisjointSets areIntersectingSets
+#'   areSameLength areSetEqual assert bapply hasLength hasNames hasLength
+#'   hasRows hasRownames hasValidDimnames hasValidNames isAFile isAlpha isAny
+#'   isCharacter isFlag isGGScale isHeaderLevel isInRange isInt isNegative
+#'   isNonNegative isNumber isPercentage isPositive isScalar isString isSubset
+#'   validate
 #' @importFrom methods as is new prototype setClass setGeneric setValidity show
 #'   slot slot<- validObject .hasSlot
 "_PACKAGE"
+
+
+
+#' @name params
+#' @inherit AcidRoxygen::params return title
+#' @keywords internal
+#'
+#' @param DESeqDataSet `DESeqDataSet` or `NULL`.
+#' @param DESeqTransform `DESeqTransform`.
+#' @param contrast `character`.
+#'   A character vector with exactly 3 elements:
+#'
+#'   1. Name of factor in the design formula.
+#'   2. Name of numerator level for the fold change.
+#'   3. Name of denominator level for the fold change.
+#'
+#'   See [DESeq2::results()] for details.
+#' @param contrastSamples `logical(1)`.
+#'   **Experimental**. Only include the samples used to define the contrast
+#'   passed to [DESeq2::results()]. This setting will break for complex DESeq2
+#'   contrasts (e.g. interaction effect).
+#' @param i `character(1)` or `integer(1)`.
+#'   `DESeqResults` contrast name or position in `results` slot.
+#' @param lfcShrink `logical(1)` or `NULL`.
+#'   Use shrunken log2 fold change (LFC) values.
+#'   If `NULL`, inherits value defined in [lfcShrink()].
+#' @param limits `list(2)`.
+#'   Named list containing `"x"` and `"y"` that define the lower and upper
+#'   limits for each axis. Set automatically by default when left `NULL`.
+#' @param name `character(1)`.
+#'   Name of the individual effect (coefficient) for building a results table.
+#'   Use this argument rather than `contrast` for continuous variables.
+#' @param results `character(1)` or `integer(1)`.
+#'   Name or position of `DESeqResults`.
+NULL
