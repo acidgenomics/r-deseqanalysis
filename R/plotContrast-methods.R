27f5b83808003f3aebefb70f36aae17ec168859b
Michael Steinbaugh <mike@steinbaugh.com>
2021-08-02 18:13:42 -0400

    Draft update to new `plotContrast` function

--- R/plotContrast-methods.R
+++ R/plotContrast-methods.R
@@ -1,8 +1,14 @@
+## FIXME Need to support plotVolcano colors here...
+## FIXME Consider supporting directionality labeling.
+
+
+
 #' Plot differential expression contrast
 #'
 #' @name plotContrast
-#'
 #' @note Updated 2021-08-02.
+#'
+#' @inheritParams params
 #'
 #' @examples
 #' data(deseq)
@@ -15,32 +21,52 @@ NULL
 
 
 ## FIXME Consider adding a "geom" argument here, for different types of plots.
+## FIXME How to return the contrast groups here...Need a function.
 
 ## Updated 2021-08-02.
 `plotContrast,DESeqAnalysis` <-
     function(object, i) {
         validObject(object)
         res <- results(object, i = i)
-        samples <- contrastSamples(
+        ## FIXME This needs to stash more metadata...
+        ## FIXME Needs to return "group", "numerator" name, "denominator" name.
+        samplesList <- contrastSamples(
             object = object,
             i = i,
-            quiet = TRUE
+            quiet = TRUE,
+            return = "list"
         )
+        assert(
+            is.list(samplesList),
+            identical(names(samplesList), c("numerator", "denominator"))
+        )
+        samples <- c(samplesList[["numerator"]], samplesList[["denominator"]])
         dds <- as(object, "DESeqDataSet")
         dds <- dds[, samples, drop = FALSE]
         counts <- counts(dds, normalized = TRUE)
         ## Only include non-zero counts on the plot.
         keep <- rowSums(counts) > 0L
         counts <- counts[keep, , drop = FALSE]
-
-        ## FIXME Need to use updated `contrastSamples` function here
-        ## to get "numerator" and "denominator" samples.
-        ##
-        ## FIXME Then we can use these mappings to calculate mean values
-        ## per grouping.
-
         ## Take the mean of the non-zero counts, per contrast.
         logcounts <- log2(counts + 1L)
+        ## FIXME Need to label DEGs here....
+        ## FIXME Use "isDEG" column here (see plotMA code).
+        data <- data.frame(
+            "x" = rowMeans(logcounts[, samplesList[["denominator"]]]),
+            "y" = rowMeans(logcounts[, samplesList[["numerator"]]]),
+            row.names = rownames(counts)
+        )
+        p <- ggplot(
+            data = data,
+            mapping = aes(
+                x = !!sym("x"),
+                y = !!sym("y")
+                ## fill = !!sym("isDEG")
+            )
+        ) +
+            geom_point(size = 1L)
+
+
 
         stop("FIXME Incomplete")
     }
