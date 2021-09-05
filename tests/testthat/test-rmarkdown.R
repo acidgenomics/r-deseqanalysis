4d72221dd08ac197ba132f676fc4504a839632c1
Michael Steinbaugh <mike@steinbaugh.com>
2021-09-05 11:25:59 -0400

    Add FIXME comment

--- tests/testthat/test-rmarkdown.R
+++ tests/testthat/test-rmarkdown.R
@@ -1,5 +1,8 @@
 context("R Markdown")
 
+## FIXME Need to improve the tempdir handling here, and clean up on success.
+## FIXME Rework using similar approach to bcbioRNASeq tests-extra f1000.R
+
 test_that("Differential expression", {
     skeleton <- system.file(
         "rmarkdown",
