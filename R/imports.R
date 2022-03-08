5290b184d386d98d91d574c83cc7d9055fa3632d
Michael Steinbaugh <mike@steinbaugh.com>
2022-03-08 07:48:21 -0500

    Rework NAMESPACE

---
+++ R/imports.R
@@ -0,0 +1,82 @@
+## FIXME Need to rework grid approach:
+##       draw_plot, ggraw
+##       Not using these any more, in favor of patchwork.
+
+
+
+## S4 classes ==================================================================
+
+#' @importClassesFrom AcidBase missingOrNULL
+#' @importClassesFrom AcidGenomes Tx2Gene
+#' @importClassesFrom DESeq2 DESeqDataSet DESeqTransform
+#' @importClassesFrom IRanges DataFrameList
+#' @importClassesFrom SummarizedExperiment RangedSummarizedExperiment
+#'   SummarizedExperiment
+NULL
+
+
+
+## S4 generics and methods =====================================================
+
+#' @importFrom AcidGenerics Gene2Symbol autopadZeros camelCase
+#'   convertGenesToSymbols convertSampleIDsToNames interestingGroups<-
+#'   intersectionMatrix leftJoin makeNames mapGenesToSymbols melt metadata2
+#'   metadata2<- plotCounts plotHeatmap plotUpset removeNA snakeCase
+#' @importFrom BiocGenerics as.data.frame cbind counts design do.call plotPCA
+#' @importFrom DESeq2 priorInfo
+#' @importFrom S4Vectors complete.cases cor decode droplevels head mcols mcols<-
+#'   metadata metadata<- na.omit relevel
+#' @importFrom SummarizedExperiment assays assays<- colData colData<- rowData
+#' @importFrom methods coerce show
+#' @importFrom pipette import
+#'
+#' @importMethodsFrom AcidBase intersectionMatrix
+#' @importMethodsFrom AcidExperiment Gene2Symbol autopadZeros
+#'   convertGenesToSymbols convertSampleIDsToNames interestingGroups<-
+#'   mapGenesToSymbols melt
+#' @importMethodsFrom AcidGenomes Gene2Symbol
+#' @importMethodsFrom AcidPlots plotCounts plotHeatmap plotPCA plotUpset
+#' @importMethodsFrom AcidPlyr leftJoin melt
+#' @importMethodsFrom DESeq2 counts design
+#' @importMethodsFrom pipette coerce import metadata2 metadata2<- removeNA
+#' @importMethodsFrom syntactic autopadZeros camelCase makeNames snakeCase
+NULL
+
+
+
+## S3 generics =================================================================
+
+#' @importFrom pipette as_tibble
+#' @importFrom stats model.matrix
+NULL
+
+
+
+## Standard functions ==========================================================
+
+#' @importFrom AcidBase initDir methodFunction printString realpath
+#'   requireNamespaces showSlotInfo standardizeCall
+#' @importFrom AcidCLI abort alert alertInfo alertWarning dl toInlineString
+#' @importFrom AcidMarkdown markdownHeader markdownList
+#' @importFrom AcidPlots !! acid_coord_flip acid_geom_label_repel
+#'   autoDiscreteColorScale autoDiscreteFillScale matchLabels
+#'   pretty_breaks sym wrap_plots
+#' @importFrom DESeq2 DESeq fpkm nbinomWaldTest
+#' @importFrom IRanges DataFrameList
+#' @importFrom S4Vectors DataFrame SimpleList
+#'
+#' @importFrom ggplot2 aes annotation_logticks element_blank geom_bar
+#'   geom_density geom_hline geom_point geom_text geom_vline ggplot guides labs
+#'   position_stack scale_color_manual scale_x_continuous scale_y_continuous
+#'   theme
+#' @importFrom goalie allArePositive areDisjointSets areIntersectingSets
+#'   areSameLength areSetEqual assert bapply hasColnames hasLength hasNames
+#'   hasLength hasRows hasRownames hasValidDimnames hasValidNames isAFile
+#'   isAlpha isAny isCharacter isFlag isHeaderLevel isInRange isInt
+#'   isMatchingFixed isNegative isNonNegative isNumber isPercentage isPositive
+#'   isScalar isString isSubset validate
+#' @importFrom methods as is new prototype setClass setGeneric setValidity slot
+#'   slot<- validObject .hasSlot
+#' @importFrom stringr str_locate_all str_match str_trunc
+#' @importFrom utils capture.output getS3method packageName packageVersion
+NULL
