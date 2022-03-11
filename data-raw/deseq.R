530a393cef2bcc05aa4055db8b5f9bec65fe5352
Michael Steinbaugh <mike@steinbaugh.com>
2022-03-11 14:42:21 -0500

    Resave example object

--- data-raw/deseq.R
+++ data-raw/deseq.R
@@ -1,37 +1,30 @@
-#' Example DESeq2 differential expression analysis.
-#' Updated 2020-03-16.
-
-library(usethis)
-library(pryr)      # 0.1.4
-library(basejump)  # 0.12.4
-library(DESeq2)    # 1.26.0
-library(apeglm)    # 1.8.0
-
-stopifnot(packageVersion("AcidTest") >= "0.3.0")
+suppressPackageStartupMessages({
+    library(devtools)
+    library(usethis)
+    library(basejump)
+    library(DESeq2)
+    library(apeglm)
+})
+load_all()
 data(RangedSummarizedExperiment, package = "AcidTest")
 rse <- RangedSummarizedExperiment
-
 ## Restrict to 2 MB.
 ## Use `pryr::object_size()` instead of `utils::object.size()`.
-limit <- structure(2e6, class = "object_size")
-
+limit <- structure(2e6L, class = "object_size")
 ## Requiring rich metadata to test `resultsTables()` and `topTables()`.
 stopifnot(
     all(c("broadClass", "description") %in% colnames(rowData(rse))),
     "condition" %in% names(colData(rse))
 )
-
-## DESeqDataSet
-rse$treatment <- as.factor(x = rep(c("C", "D"), each = 3L))
+## Generate DESeqDataSet.
+colData(rse)[["treatment"]] <- as.factor(x = rep(c("C", "D"), each = 3L))
 design <- ~ condition + treatment + condition:treatment
 dds <- DESeqDataSet(se = rse, design = design)
 dds <- DESeq(dds)
 validObject(dds)
-
-## DESeqTransform
+## Generate DESeqTransform.
 dt <- varianceStabilizingTransformation(dds)
-
-## DESeqResults list
+## Generate DESeqResults list.
 alpha <- 0.01
 lfcThreshold <- 0L
 contrasts <- list(
@@ -57,10 +50,9 @@ res <- mapply(
     SIMPLIFY = FALSE,
     USE.NAMES = TRUE
 )
-
-## Shrink log2 fold changes via `DESeq2::lfcShrink()`.
-## apeglm is now recommended over normal. Also note that LFC shrinkage via
-## type='normal' is not implemented for designs with interactions.
+## Shrink log2 fold changes via `DESeq2::lfcShrink()`. apeglm is now recommended
+## over normal. Also note that LFC shrinkage via type='normal' is not
+## implemented for designs with interactions.
 shrink <- mapply(
     FUN = apeglmResults,
     contrast = contrasts,
@@ -72,7 +64,6 @@ shrink <- mapply(
     SIMPLIFY = FALSE,
     USE.NAMES = TRUE
 )
-
 ## Package up the analysis into a DESeqAnalysis object.
 deseq <- DESeqAnalysis(
     data = dds,
@@ -80,14 +71,10 @@ deseq <- DESeqAnalysis(
     results = res,
     lfcShrink = shrink
 )
-
 ## Check the object size.
-lapply(coerceS4ToList(deseq), object_size)
-object_size(deseq)
-stopifnot(object_size(deseq) < limit)
-
+lapply(coerceToList(deseq), object.size)
+stopifnot(object.size(deseq) < limit)
 ## Check that object is valid.
 stopifnot(is(deseq, "DESeqAnalysis"))
 validObject(deseq)
-
 use_data(deseq, overwrite = TRUE, compress = "xz")
