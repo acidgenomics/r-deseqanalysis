248444539eb13ed6ad8b0f983d9b9ab02d42b309
Michael Steinbaugh <mike@steinbaugh.com>
2022-03-30 15:23:10 -0400

    Migrate FIXMEs into todo

--- R/plotMA-methods.R
+++ R/plotMA-methods.R
@@ -84,8 +84,6 @@ NULL
             )
         }
         if (isCharacter(genes) || isTRUE(isPositive(ntop))) {
-            ## FIXME Need to handle NA gene symbols here.
-            ## FIXME Need to get this against a better example dataset.
             dds <- convertGenesToSymbols(dds)
             rownames(res) <- rownames(dds)
         }
