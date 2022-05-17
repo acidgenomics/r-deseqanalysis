cae0dcc526436ac78c3d08504a182f72aa6a5fa0
Michael Steinbaugh <mike@steinbaugh.com>
2022-05-17 11:09:19 -0400

    Resave example object

--- R/data.R
+++ R/data.R
@@ -1,27 +1,25 @@
-#' List of DESeqDataSet example objects
-#'
-#' @author Michael Steinbaugh
-#' @note Updated 2021-06-29.
-#'
-#' @usage data(dds_list)
-#'
-#' @examples
-#' data(dds_list)
-#' dds_list
-"dds_list"
-
-
-
 #' DESeq2 example analysis
 #'
-#' Minimal DESeq2 differential expression analysis.
-#'
 #' @author Michael Steinbaugh
-#' @note Updated 2021-06-29.
+#' @note Updated 2022-05-17.
 #'
 #' @usage data(deseq)
 #'
 #' @examples
 #' data(deseq)
-#' deseq
+#' print(deseq)
 "deseq"
+
+
+
+#' Minimal DESeq2 example analysis
+#'
+#' @author Michael Steinbaugh
+#' @note Updated 2022-05-17.
+#'
+#' @usage data(deseqMinimal)
+#'
+#' @examples
+#' data(deseqMinimal)
+#' print(deseqMinimal)
+"deseqMinimal"
