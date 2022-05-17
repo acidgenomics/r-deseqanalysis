710620dde8e613133c31a3e35e3367134c897778
Michael Steinbaugh <mike@steinbaugh.com>
2022-05-17 11:19:36 -0400

    Tighten code coverage against minimal DESeqAnalysis object

--- tests/testthat/helper-globals.R
+++ tests/testthat/helper-globals.R
@@ -1,11 +1,16 @@
-data(deseq, envir = environment())
-
-dds <- as(deseq, "DESeqDataSet")
-g2s <- basejump::Gene2Symbol(dds)
-geneIds <- head(g2s[["geneId"]])
-geneNames <- head(g2s[["geneName"]])
-
 ## nolint start
+Gene2Symbol <- AcidGenomes::Gene2Symbol
 realpath <- basejump::realpath
 render <- rmarkdown::render
 ## nolint end
+
+tmpenv <- new.env()
+data(deseq, deseqMinimal, envir = tmpenv)
+objs <- list()
+objs[["deseq"]] <- get("deseq", envir = tmpenv)
+objs[["deseqMinimal"]] <- get("deseqMinimal", envir = tmpenv)
+rm(tmpenv)
+objs[["dds"]] <- as(objs[["deseq"]], "DESeqDataSet")
+objs[["g2s"]] <- Gene2Symbol(objs[["dds"]])
+objs[["geneIds"]] <- head(objs[["g2s"]][["geneId"]])
+objs[["geneNames"]] <- head(objs[["g2s"]][["geneName"]])
--- tests/testthat/test-topTables.R
+++ tests/testthat/test-topTables.R
@@ -1,17 +1,20 @@
 context("topTables")
 
 test_that("DESeqAnalysis", {
-    x <- capture.output(topTables(deseq, i = 1L))
-    expect_true(any(grepl("\\|padj", x)))
+    for (object in objs[c("deseq", "deseqMinimal")]) {
+        x <- capture.output(topTables(object, i = 1L))
+        expect_true(any(grepl("\\|padj", x)))
+    }
 })
 
 test_that("DESeqResults", {
+    deseq <- objs[["deseq"]]
     res <- results(deseq, i = 1L)
     dds <- as(deseq, "DESeqDataSet")
     ## Minimal return.
     x <- capture.output(topTables(res))
     expect_false(any(grepl("\\|geneName", x)))
     ## Extra mode, using DESeqDataSet.
-    x <- capture.output(topTables(res, DESeqDataSet = dds))
+    x <- capture.output(topTables(object = res, DESeqDataSet = dds))
     expect_true(any(grepl("\\|geneName", x)))
 })
