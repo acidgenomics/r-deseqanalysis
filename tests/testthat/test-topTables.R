60ab33ad03d7a3b32241007c63ba74608346aa84
Michael Steinbaugh <mike@steinbaugh.com>
2022-05-17 16:47:21 -0400

    Improve code coverage

--- tests/testthat/test-topTables.R
+++ tests/testthat/test-topTables.R
@@ -1,20 +1,39 @@
 test_that("DESeqAnalysis", {
     object <- objs[["deseq"]]
     x <- capture.output(topTables(object, i = 1L))
-    expect_true(any(grepl("\\|padj", x)))
+    x <- x[[5L]]
+    x <- gsub(pattern = " ", replacement = "", x = x)
+    x <- strsplit(x = x, split = "|", fixed = TRUE)[[1L]]
+    expect_identical(
+        object = x,
+        expected = c(
+            "",
+            "",
+            "baseMean",
+            "lfc",
+            "padj",
+            "broadClass",
+            "geneName",
+            "description"
+        )
+    )
+    object <- objs[["deseq"]]
+    rowData(object@data) <- NULL
+    x <- capture.output(topTables(object, i = 1L))
+    x <- x[[5L]]
+    x <- gsub(pattern = " ", replacement = "", x = x)
+    x <- strsplit(x = x, split = "|", fixed = TRUE)[[1L]]
+    expect_identical(
+        object = x,
+        expected = c(
+            "",
+            "",
+            "baseMean",
+            "lfc",
+            "padj"
+        )
+    )
     object <- objs[["deseqMinimal"]]
     x <- capture.output(topTables(object, i = 1L))
-    expect_false(any(grepl("\\|padj", x)))
-})
-
-test_that("DESeqResults", {
-    deseq <- objs[["deseq"]]
-    res <- results(deseq, i = 1L)
-    dds <- as(deseq, "DESeqDataSet")
-    ## Minimal return.
-    x <- capture.output(topTables(res))
-    expect_false(any(grepl("\\|geneName", x)))
-    ## Extra mode, using DESeqDataSet.
-    x <- capture.output(topTables(object = res, DESeqDataSet = dds))
-    expect_true(any(grepl("\\|geneName", x)))
+    expect_identical(x, character())
 })
