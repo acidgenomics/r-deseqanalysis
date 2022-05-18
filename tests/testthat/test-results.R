1f18a67b322760f8d3fcfca2bd027b9f04109559
Michael Steinbaugh <mike@steinbaugh.com>
2022-05-18 08:26:02 -0400

    Improve code coverage

--- tests/testthat/test-results.R
+++ tests/testthat/test-results.R
@@ -1,13 +1,72 @@
-## FIXME Add code coverage for `extra = TRUE` for very minimal DESeqDataSet.
-
-test_that("results argument", {
+test_that("DESeqAnalysis", {
     object <- objs[["deseq"]]
-    ## Numeric scalar.
     x <- results(object, i = 1L)
     expect_s4_class(x, "DESeqResults")
-    ## Character string.
     x <- results(object, i = resultsNames(object)[[1L]])
     expect_s4_class(x, "DESeqResults")
+    x <- results(object, i = 1L, extra = TRUE)
+    expect_identical(
+        object = colnames(x),
+        expected = c(
+            "baseMean",
+            "log2FoldChange",
+            "lfcSE",
+            "stat",
+            "pvalue",
+            "padj",
+            "broadClass",
+            "description",
+            "geneBiotype",
+            "geneId",
+            "geneIdNoVersion",
+            "geneIdVersion",
+            "geneName",
+            "sample1",
+            "sample2",
+            "sample3",
+            "sample4",
+            "sample5",
+            "sample6",
+            "sample7",
+            "sample8",
+            "sample9",
+            "sample10",
+            "sample11",
+            "sample12"
+        )
+    )
+    x <- results(object, i = 1L, extra = FALSE)
+    expect_identical(
+        object = colnames(x),
+        expected = c(
+            "baseMean",
+            "log2FoldChange",
+            "lfcSE",
+            "stat",
+            "pvalue",
+            "padj"
+        )
+    )
+})
+
+test_that("DESeqAnalysis without rowData", {
+    object <- objs[["deseqMinimal"]]
+    x <- results(object, i = 1L, extra = TRUE)
+    expect_identical(
+        object = colnames(x),
+        expected = c(
+            "baseMean",
+            "log2FoldChange",
+            "lfcSE",
+            "stat",
+            "pvalue",
+            "padj",
+            "sample1",
+            "sample2",
+            "sample3",
+            "sample4"
+        )
+    )
 })
 
 test_that("Match failure", {
